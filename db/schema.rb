0d3d739f6d5b9bf80603ded2c4ecf0c996746106
Ilse Alejo <ilse.alejo@gmail.com>
2015-12-29 20:12:45 -0600

    Added dog seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,3 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20151229231617) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "breed"
+    t.string   "specify"
+    t.text     "description"
+    t.text     "color"
+    t.string   "gender"
+    t.integer  "age"
+    t.string   "size"
+    t.boolean  "sterilized",           default: false
+    t.boolean  "vaccined",             default: false
+    t.boolean  "urgent",               default: false
+    t.date     "adopt_before"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.integer  "adopted",              default: -1
+    t.string   "photo1"
+    t.string   "photo2"
+    t.string   "photo3"
+    t.text     "adopter_requirements"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+end
