7da2c98bb0459be5ee968b28dfc37e8906a4b8a1
Ilse Alejo <ilse.alejo@gmail.com>
2015-12-30 19:01:22 -0600

    Created method for adoption, confirmation and unconfirmation and hide the edit and delete links

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
 	before_action :set_dog, only: [:show, :edit, :update, :destroy]
+	before_action :set_dog_for_adoption, only: [:adoption, :confirm_adoption, :unconfirm_adoption]
 	def index
 		@dogs = Dog.where(:adopted => -1).paginate(:page => params[:page], :per_page => 20)
 	end
@@ -43,6 +44,26 @@ class DogsController < ApplicationController
 		redirect_to root_url
 	end
 
+	def adoption
+		@dog.update_attributes( :adopted => 0 )
+		@dog.update_attributes( :adopter_id => current_user.email )
+		flash[:notice] = "We send you an email with the contact data of the owner, we also sent an email to the owner with your contact data"
+		redirect_to @dog
+	end
+
+	def confirm_adoption
+		@dog.update_attributes( :adopted => 1 )
+		flash[:notice] = "Yes! We are generating the certificate."
+		redirect_to @dog
+
+	end
+
+	def unconfirm_adoption
+		@dog.update_attributes( :adopted => -1 )
+		@dog.update_attributes( :adopter_id => nil )
+		redirect_to @dog
+	end
+
 	private
 		def dog_params
 			params.require(:dog).permit(:name, :description, :color, :gender, :age, :size, :sterilized, :vaccined, :urgent, :city, :state, :country, :photo1, :photo2, :photo3, :specify, :breed, :adopter_requirements)
@@ -50,6 +71,13 @@ class DogsController < ApplicationController
 
 		def set_dog
 			@dog = Dog.find(params[:id])
+			rescue  ActiveRecord::RecordNotFound
+			flash[:alert] = "This isn't the dog you are looking for."
+			redirect_to root_url
+		end
+
+		def set_dog_for_adoption
+			@dog = Dog.find(params[:dog_id])
 			rescue  ActiveRecord::RecordNotFound
 			flash[:alert] = "This isn't the dog you are looking for."
 			redirect_to root_url
